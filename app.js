48275a475a84d005f8ea5c608827276a04be36b4
Shivam Dwivedi <64479884+sparrow-code@users.noreply.github.com>
2023-06-21 15:16:13 +0530

    S2

---
+++ app.js
@@ -0,0 +1,62 @@
+const fs = require("fs");
+const { Client, LocalAuth } = require("whatsapp-web.js");
+
+const qrcode = require("qrcode-terminal");
+
+const xlsx = require("xlsx");
+
+const workbook = xlsx.readFile("./data.xlsx");
+
+// Get the first sheet
+const sheet = workbook.Sheets[workbook.SheetNames[0]];
+
+let validValues = [];
+
+const client = new Client({
+  authStrategy: new LocalAuth(),
+});
+
+client.on("qr", (qr) => {
+  // Generate and scan this code with your phone
+  qrcode.generate(qr, { small: true });
+});
+
+client.on("ready", () => {
+  console.log("Client is ready!");
+
+  client.sendMessage("919696227984@c.us", "Bot is Ready");
+
+  // Iterate over the sheet rows
+  for (const cellAddress in sheet) {
+    // Access the cell value
+    const cellValue = sheet[cellAddress].v;
+    if (typeof cellValue === "number" && cellValue.toString().length === 10) {
+      validValues.push(cellValue);
+    }
+  }
+});
+
+// 919696227984@c.us
+
+client.on("message", (msg) => {
+  if (msg.body == "!ping") {
+    msg.reply("pong");
+  }
+  if (msg.body == "!promote") {
+    
+    // client.sendMessage(`91${validValues[i]}@c.us`, "Hi How Are You");
+    msg.reply("Message Have Been Start Promoting 🎉");
+  }
+});
+
+/* client.on('authenticated', (session) => {
+  sessionData = session;
+  fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), (err) => {
+      if (err) {
+          console.error(err);
+      }
+  });
+}); */
+
+// Start the client
+client.initialize();
